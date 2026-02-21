aa6b520ca2408fadb9e8bc366897286ee137ea66
Maria Clara Rezende Vianna <97710400+MariaClaraRVN@users.noreply.github.com>
2026-02-21 01:19:26 -0300

    ajuste do tamanho da logo

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -186,7 +186,7 @@ export default function Index() {
           <div className="flex items-center gap-2">
             {/* <span className="text-white font-black text-2xl tracking-wide">L</span>
             <span className="text-[hsl(var(--brand-green-light))] font-black text-2xl">⚡M MANUTENÇÕES</span> */}
-            <img tyle={{maxWidth: "200px", margin: "0 auto"}} src="/Icon.jpg" alt="Logo LM" />
+            <img style={{maxWidth: "150px", margin: "0 auto"}} src="/Icon.jpg" alt="Logo LM" />
           </div>
           <div className="flex items-center gap-4">
             <span className="text-sm text-gray-400 hidden sm:block">Sistema de Orçamentos</span>
